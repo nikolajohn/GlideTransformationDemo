1b9e3b2153f2409c7e916cc1c193fddb55340b12
nikolajohn <2749068735@qq.com>
2018-10-21 00:34:04 +0800

    Modify Parameters and prestall back image to achieve smoother display effect

--- app/src/main/java/cn/edu/zju/glidetest/BGtestActivity.java
+++ app/src/main/java/cn/edu/zju/glidetest/BGtestActivity.java
@@ -4,7 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
+
 public class BGtestActivity extends AppCompatActivity {
+    private ImageView backimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +19,12 @@ public class BGtestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bgtest);
         ImageView backimage = findViewById(R.id.back);
 
-        /*Glide.with(this)
+        /*backimage.buildDrawingCache();
+        Bitmap sentBitmap = backimage.getDrawingCache();
+        backimage.setImageBitmap(BlurBitmapUtil.blurBitmap(BGtestActivity.this, backimage.getDrawingCache(), 20.0f));*/
+        Glide.with(this)
                 .load(R.mipmap.back)
-                .apply(bitmapTransform(new BlurTransformation(250)))
-                .into(backimage);*/
+                .apply(bitmapTransform(new BlurTransformation(80)))
+                .into(backimage);
     }
 }
